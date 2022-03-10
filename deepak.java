75f5ad36932ae46fc860f58f7b635b5157db62fd
deepak <deepak2000kasi@gmail.com>
2022-03-10 15:28:46 +0530

    modified float value as 2

--- deepak.java
+++ deepak.java
@@ -3,7 +3,7 @@ public class deepak {
    
 
    public static void main(String []args) {
-      float myfloatnum=10.99f;
+      float myfloatnum=2.99f;
       System.out.println(myfloatnum); 
    }
 }
